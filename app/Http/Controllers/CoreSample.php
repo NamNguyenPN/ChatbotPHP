c9e7d8ef2ec22c76b70f3e7fb638e316da72587d
NamNguyenPN <abc2341999@gmail.com>
2021-08-16 19:49:23 +0700

    Main Source

--- app/Http/Controllers/CoreSample.php
+++ app/Http/Controllers/CoreSample.php
@@ -30,8 +30,7 @@ class CoreSample extends Controller
         $text="Chào bạn";        
         
         $response = $this->GetJson->MessageJSON($sender_id,$text);
-        $url="https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.6435-9/117958532_976844066117437_5419867200041842242_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=C9Ci7NNe3AkAX_zDDmy&_nc_ht=scontent.fsgn2-3.fna&oh=6f63d928dd8511a63b1e07d82f928ed8&oe=613FB77C";
-        $response = $this->GetJson->AttachementJSON($sender_id,"image",$url);
+        
         $this->SendMessage->Send($response);
 
         // $menu = $this->GetJson->MainMenuJSON($sender_id);
