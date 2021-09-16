24884efabab28ab7144f16947a3bb67fbf2abc20
NamNguyenPN <abc2341999@gmail.com>
2021-09-16 19:50:59 +0700

    update main

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -10,13 +10,13 @@ class SendMessage extends Controller
 {
     public function go($response)
     {
-        $url='https://graph.facebook.com/v11.0/me/messages?access_token='.env('PAGE_ACCESS_TOKEN');
+        $url='https://graph.facebook.com/v12.0/me/messages?access_token='.env('PAGE_ACCESS_TOKEN');
         $res = Http::post($url,json_decode($response,true));
         error_log("Status: ".$res->status());
     }
     public function menu($response)
     {
-        $url='https://graph.facebook.com/v11.0/me/messenger_profile?access_token='.env('PAGE_ACCESS_TOKEN');
+        $url='https://graph.facebook.com/v12.0/me/messenger_profile?access_token='.env('PAGE_ACCESS_TOKEN');
         $res = Http::post($url,json_decode($response,true));
         error_log("Status: ".$res->status());
     }
